ff654f3f9c16416649b5007677d3e7e484751bbf
Tony Theodoropoulos <theodot@outlook.com>
2020-02-14 13:47:20 -0500

    Add project files.

---
+++ LoginProgram/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace LoginProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userInput = new System.Windows.Forms.TextBox();
+            this.passInput = new System.Windows.Forms.TextBox();
+            this.userLabel = new System.Windows.Forms.Label();
+            this.passLabel = new System.Windows.Forms.Label();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // userInput
+            // 
+            this.userInput.Location = new System.Drawing.Point(217, 69);
+            this.userInput.Name = "userInput";
+            this.userInput.Size = new System.Drawing.Size(100, 20);
+            this.userInput.TabIndex = 0;
+            // 
+            // passInput
+            // 
+            this.passInput.Location = new System.Drawing.Point(217, 127);
+            this.passInput.Name = "passInput";
+            this.passInput.Size = new System.Drawing.Size(100, 20);
+            this.passInput.TabIndex = 1;
+            // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.Location = new System.Drawing.Point(140, 76);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(53, 13);
+            this.userLabel.TabIndex = 2;
+            this.userLabel.Text = "username";
+            // 
+            // passLabel
+            // 
+            this.passLabel.AutoSize = true;
+            this.passLabel.Location = new System.Drawing.Point(140, 130);
+            this.passLabel.Name = "passLabel";
+            this.passLabel.Size = new System.Drawing.Size(52, 13);
+            this.passLabel.TabIndex = 3;
+            this.passLabel.Text = "password";
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.Color.Tan;
+            this.loginButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Fuchsia;
+            this.loginButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loginButton.Location = new System.Drawing.Point(242, 190);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 4;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(143, 250);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(35, 13);
+            this.outputLabel.TabIndex = 5;
+            this.outputLabel.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(242, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 324);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passLabel);
+            this.Controls.Add(this.userLabel);
+            this.Controls.Add(this.passInput);
+            this.Controls.Add(this.userInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox userInput;
+        private System.Windows.Forms.TextBox passInput;
+        private System.Windows.Forms.Label userLabel;
+        private System.Windows.Forms.Label passLabel;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ LoginProgram/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginProgram
+{
+    public partial class Form1 : Form
+    {
+        string username;
+        int password;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                loginButton.BackColor = Color.Yellow;
+
+                username = userInput.Text;
+                password = Convert.ToInt16(passInput.Text);
+
+                outputLabel.Text = $"Welcome {username}";
+            }
+            catch
+            {
+                outputLabel.Text = "Input error";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            outputLabel.Text = $"Welcome {username}";
+        }
+    }
+}
